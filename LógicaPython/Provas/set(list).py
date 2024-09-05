d7dba6b650af557303c67cf903d643691abb8042
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2024-09-05 19:58:58 -0300

    Delete LógicaPython/Provas/set(list).py

--- LógicaPython/Provas/set(list).py
+++
@@ -1,7 +0,0 @@
-# Exibe no terminal o numero ou elemento que apareceu mais vezes.
-list = [
-    1,2,3,4,4,4,2,2,5,6,7,7,8,9,9,9,4,4,4,1,1,2,2,2,2,3
-]
-
-frenquente = max(set(list), key=list.count)
-print(frenquente)
