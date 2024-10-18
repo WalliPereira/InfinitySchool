0ed4955c21d28b5f28b39daffa37d0cd8c0112ab
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2024-10-17 21:02:49 -0300

    Add files via upload

---
+++ PYIA - Python IA/Provas/Prova001/app.py
@@ -0,0 +1,13 @@
+# Crie uma lista contendo seis frutas de sua escolha.
+# Depois de ter a lista pronta, converta essa lista em uma tupla.
+# Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+frutas = ['Pera',
+          'Maça',
+          'Morango',
+          'Banana',
+          'Abacaxi',
+          'Abacate',
+          ]
+tupla = tuple(frutas)
+print(tupla)
