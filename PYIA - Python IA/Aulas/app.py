43ac8d2a5038feb6a2aee89605842b3c3136a728
Wallisson Pereira <157427235+WalliPereira@users.noreply.github.com>
2024-10-20 17:04:22 -0300

    Add files via upload

---
+++ PYIA - Python IA/Aulas/app.py
@@ -0,0 +1,13 @@
+# Ex03. Faça um programa que peça o nome, 3 notas de um aluno, e armazene-os em um dicionário, também armazene nesse dicionário a média e a situação do aluno (>= 6 está aprovado, se não reprovado.)
+
+dicionario = {}
+
+dicionario['nome'] = input("Digite seu nome: ")
+
+for i in range(1, 4):
+    dicionario[f'nota{i}'] = float(input(f"Informe a nota {i}: "))
+    
+dicionario['media'] = (dicionario['nota1']+dicionario['nota2']+dicionario['nota3'])/3
+
+print(dicionario)
+
